86ceafdc2d9a900498239d6854970044fcf879f0
Sascha Depold <sascha@depold.com>
2015-02-25 21:33:20 +0100

    Add npm_bump as shell script

---
+++ CHANGELOG.md
@@ -0,0 +1,10 @@
+# Change Log
+All notable changes to this project will be documented in this file.
+
+## Upcoming
+### Changed
+- Introduce bash scripts
+
+## 1.0.0 - 2015-02-25
+### Added
+- Initial versions written in Ruby
---
+++ bin/npm_bump.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+VERSION=$1
+
+if [ -z $VERSION ]; then
+  echo "No version specified. Expected command call: $0 1.2.3"
+  exit 1
+fi
+
+if [ -f CHANGELOG.md ]; then
+  grep '## Upcoming' CHANGELOG.md > /dev/null
+
+  if [ "$?" == 0 ]; then
+    printf "Modifying change log ... "
+    sed -i '' 's/Upcoming/1.2.3/' CHANGELOG.md
+    git add CHANGELOG.md
+    git commit -m "Add changes for version: v$VERSION"
+    echo "done."
+  fi
+fi
+
+printf "Bumping package version ... "
+npm version $VERSION -m "Bump to version: v$VERSION"
+echo "done."
+
+git push --tags
